98b8004f3ee4bdb4a5cc3ec188c6d34eda8cc5ca
KingstonBKG <bakobogalbertlegrand@gmail.com>
2025-03-06 12:03:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const allowedOrigins = [
+  'http://localhost:3000',];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+
+// Gestion des routes non trouvées pour l'API
+app.get('/api/*', (req, res) => {
+  res.status(404).json({ error: 'API route not found' });
+});
+
+
+// Démarrage du serveur
+app.listen(3000, () => {
+  console.log(`Serveur démarré sur le port http://localhost:${config.port}`);
+});
